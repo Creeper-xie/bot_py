1c758ad22487cd7aa0b47e5dbf5c0ef9516c72d3
Creeper-xie <Creeperxie@outlook.com>
2025-04-11 02:42:23 +0000

    feat: Gemini回话

--- bot.py
+++ bot.py
@@ -12,21 +12,44 @@ else:
     import tomli as tomllib
 
 def ai(msg):
-    msg1= "{\"contents\": [{\"parts\":[{\"text\":" "\"" + msg+ "\"" "}]}]}"
-    print(msg1)
-    data = json.loads(msg1)
-    print(data)
-    gemini_url= "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash-exp:generateContent?key={}".format(config["api_key"])
-    resp = requests.post(gemini_url,data)
-    print(resp.text)
+    reqMsg= "{\"contents\": [{\"parts\":[{\"text\":" "\"" + msg["message"][0]["data"]["text"]+ "\"" "}]}]}"
+    gemini_url= "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash-thinking-exp:generateContent?key={}".format(config["api_key"])
+    while True:
+        try:
+            resp = requests.post(gemini_url,reqMsg)
+            print(resp.text)
+            return json.loads(resp.text)
+        except requests.exceptions.HTTPError as e:
+            print(e)
+            continue
 
 async def client(config):
     uri=config["bot_ws_uri"]
     async with connect(uri,additional_headers={"Authorization": "Bearer {}".format(config["token"])}) as websocket:
         while True:
-            msg = await websocket.recv()
-            print(msg)
-#            ai(msg)
+            msg = json.loads(await websocket.recv())
+            if  "message_type" in msg and msg["message_type"] == "private":
+                print(msg)
+                rec = ai(msg)
+#                sendMsg = json.loads("{\"action\": \"send_msg\",\"params\": {\"detail_type\":\"private\",\"user_id\":\"123456\",\"message\":[{\"type\"=\"text\",\"data\": {\"text\":\"" + rec["candidates"][0]["content"]["parts"][0]["text"] + "\"}}]}}")
+                sendMsg = {
+    "action": "send_msg",
+    "params": {
+        "detail_type": "private",
+        "user_id": msg["user_id"],  # 显式转换为字符串
+        "message": [
+            {
+                "type": "text",  # 修正原代码中的 = 应为 :
+                "data": {
+                    "text": rec["candidates"][0]["content"]["parts"][0]["text"]
+                }
+            }
+        ]
+    }
+}
+                await websocket.send(json.dumps(sendMsg))
+
+
 
 if __name__ == "__main__":
     with open("config.toml", "rb") as f:
